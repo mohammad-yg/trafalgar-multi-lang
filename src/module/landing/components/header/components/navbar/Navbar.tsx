43fada7355eccc712912eef54da0d017b60ce22e
mohammad yadegari <mohammadyadegari032@gmail.com>
2023-05-04 21:55:02 +0430

    add lnading media

--- src/module/landing/components/header/components/navbar/Navbar.tsx
+++ src/module/landing/components/header/components/navbar/Navbar.tsx
@@ -3,17 +3,17 @@ import { FC } from "react";
 import style from "./style.module.scss";
 
 const Navbar: FC = () => {
-  const activeItem = "Home";
-  const items = ["Home", "Find a doctor", "Apps", "Testimonials", "About us"];
-  return (
-    <>
-      <ul className={style.list}>
-        {items.map((_) => (
-          <li className={`${_ === activeItem ? style.active : ''}`}>{_}</li>
-        ))}
-      </ul>
-    </>
-  );
+    const activeItem = "Home";
+    const items = ["Home", "Find a doctor", "Apps", "Testimonials", "About us"];
+    return (
+        <>
+            <ul className={style.list}>
+                {items.map((_) => (
+                    <li className={`${_ === activeItem ? style.active : ''}`}>{_}</li>
+                ))}
+            </ul>
+        </>
+    );
 };
 
 export default Navbar;
