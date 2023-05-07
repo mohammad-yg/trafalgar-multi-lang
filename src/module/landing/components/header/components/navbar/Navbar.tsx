8d9bb5632b00942646bbeaabe34149bfdbc9be21
mohammad yadegari <mohammadyadegari032@gmail.com>
2023-05-07 19:19:04 +0430

    translate navbar

--- src/module/landing/components/header/components/navbar/Navbar.tsx
+++ src/module/landing/components/header/components/navbar/Navbar.tsx
@@ -1,20 +1,28 @@
 import { FC } from "react";
 import style from "./style.module.scss";
+import { useAppTranslator } from "@/module/layout/core/layoutContext";
 
 const Navbar: FC = () => {
-    const activeItem = "Home";
-    const items = ["Home", "Find a doctor", "Apps", "Testimonials", "About us"];
-    return (
-        <>
-            <ul className={style.list}>
-                {items.map((_, index) => (
-                    <li key={index} className={`${_ === activeItem ? style.active : ""}`}>
-                        {_}
-                    </li>
-                ))}
-            </ul>
-        </>
-    );
+  const activeItem = "Home";
+  const translator = useAppTranslator();
+  const items = [
+    "Home",
+    "Find_a_doctor",
+    "Apps",
+    "Testimonials",
+    "About_us",
+  ].map((_) => translator.translate(["landing", "header"], _));
+  return (
+    <>
+      <ul className={style.list}>
+        {items.map((_, index) => (
+          <li key={index} className={`${_ === activeItem ? style.active : ""}`}>
+            {_}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default Navbar;
